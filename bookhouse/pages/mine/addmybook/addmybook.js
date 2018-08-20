d17cd21d9b81634d40ee731c8e982049628e808e
yunteng <617423733@qq.com>
2018-08-20 21:58:41 +0800

    today bookhouse

--- bookhouse/pages/mine/addmybook/addmybook.js
+++ bookhouse/pages/mine/addmybook/addmybook.js
@@ -1,4 +1,31 @@
 // pages/mine/addmybook/addmybook.js
+//地图功能单独拿出来 -xzz1023
+var village_LBS = function(that) {
+  wx.getLocation({
+    type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+    success: function(res) {
+      // 调用接口, 坐标转具体位置 -xxz0717
+      demo.reverseGeocoder({
+        location: {
+          latitude: Number(res.latitude),
+          longitude: Number(res.longitude)
+        },
+        success: function(res) {
+          console.log(res);
+          // that.setData({
+          //   start_address: res.result.address,   //起点地址
+          //   city: res.result.address_component.city,  //起点城市
+          //   district: res.result.address_component.district   //区
+          // })
+        }
+      })
+    }
+
+
+  })
+}
+
+
 Page({
 
   /**
@@ -8,7 +35,7 @@ Page({
     title: "",
     image: "",
     author: "",
-    isbn:""
+    isbn: ""
   },
 
   /**
@@ -16,18 +43,78 @@ Page({
    */
   onLoad: function(options) {
     var that = this;
-    that.setData({
-      title: options.title,
-      image:options.image,
-      author: options.author,
-      isbn:options.isbn
+    village_LBS(that);
+
+    wx.getStorage({
+      key: 'isbn',
+      success: function(res) {
+        that.setData({
+          title: options.title,
+          image: options.image,
+          author: options.author,
+          isbn: res.data
+        })
+      },
     })
-    console.log(isbn)
+
+
+    ///console.log(isbn)
   },
-  formSubmit: function (e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value.title)
-    wx.navigateTo({
-      url: '../mybook/mybook',
+  formSubmit: function(e) {
+    var that = this;
+
+    //console.log('form发生了submit事件，携带数据为：', e.detail.value.image)
+    wx.getStorage({
+      key: 'openid',
+      success: function(res) {
+        //var openid = res.data
+        //console.log(this.image)
+        wx.request({
+          method: 'GET',
+          url: 'http://localhost:8080/kq/json/addBook',
+          header: { //请求头
+            "Content-Type": "applciation/json"
+          },
+          data: {
+            "openid": res.data,
+            "isbn": that.data.isbn,
+            "title": e.detail.value.title,
+            "author": e.detail.value.author,
+            "image": that.data.image,
+            "send_word": e.detail.value.sendword
+          },
+
+          success: function(res) {
+            console.log("用户信息到服务器success")
+            wx.navigateTo({
+              url: '../mybook/mybook',
+            })
+          }
+        })
+
+      },
+    })
+
+  },
+  selectAdd() {
+
+    wx.getLocation({
+      type: "gcj02",
+      success: function(res) {
+        wx.openLocation({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          success: function(res) {
+            wx.chooseLocation({
+              success: function(res) {
+                console.log(res)
+              },
+            })
+
+          }
+        })
+        console.log(res)
+      },
     })
   },
   /**
@@ -35,47 +122,94 @@ Page({
    */
   onReady: function() {
 
+
+    var that = this;
+    wx.getSetting({
+      success: (res) => {
+        console.log(res);
+        console.log(res.authSetting['scope.userLocation']);
+        if (res.authSetting['scope.userLocation'] != undefined && res.authSetting['scope.userLocation'] != true) { //非初始化进入该页面,且未授权
+          wx.showModal({
+            title: '是否授权当前位置',
+            content: '需要获取您的地理位置，请确认授权，否则地图功能将无法使用',
+            success: function(res) {
+              if (res.cancel) {
+                console.info("1授权失败返回数据");
+
+              } else if (res.confirm) {
+                //village_LBS(that);
+                wx.openSetting({
+                  success: function(data) {
+                    console.log(data);
+                    if (data.authSetting["scope.userLocation"] == true) {
+                      wx.showToast({
+                        title: '授权成功',
+                        icon: 'success',
+                        duration: 5000
+                      })
+                      //再次授权，调用getLocationt的API
+                      village_LBS(that);
+                    } else {
+                      wx.showToast({
+                        title: '授权失败',
+                        icon: 'success',
+                        duration: 5000
+                      })
+                    }
+                  }
+                })
+              }
+            }
+          })
+        } else if (res.authSetting['scope.userLocation'] == undefined) { //初始化进入
+          village_LBS(that);
+        }
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
 
-  },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
 
-  },
+/**
+ * 生命周期函数--监听页面显示
+ */
+onShow: function() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
+},
 
-  },
+/**
+ * 生命周期函数--监听页面隐藏
+ */
+onHide: function() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
+},
 
-  },
+/**
+ * 生命周期函数--监听页面卸载
+ */
+onUnload: function() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
+},
 
-  },
+/**
+ * 页面相关事件处理函数--监听用户下拉动作
+ */
+onPullDownRefresh: function() {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
+},
 
-  }
+/**
+ * 页面上拉触底事件的处理函数
+ */
+onReachBottom: function() {
+
+},
+
+/**
+ * 用户点击右上角分享
+ */
+onShareAppMessage: function() {
+
+}
 })
--- bookhouse/pages/mine/mine.js
+++ bookhouse/pages/mine/mine.js
@@ -17,6 +17,17 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function() {
+    //地址授权
+    // wx.openSetting({
+    //   success: (res) => {
+        
+    //      res.authSetting = {
+    //         "scope.userInfo": true,
+    //         "scope.userLocation": true
+    //       }
+    //     openSetting:fail 此接口已废弃，请使用 OpenSetting 组件
+    //   }
+    // })
 
     if (app.globalData.userInfo) {
       this.setData({
@@ -24,8 +35,6 @@ Page({
         hasUserInfo: true
       })
       
-
-
     } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
@@ -90,7 +99,10 @@ Page({
     wx.scanCode({
       success: (res) => {
         //console.log(res.result)
-         var isbn =res.result
+         wx.setStorage({
+           key: 'isbn',
+           data: res.result,
+         })
         //通过豆瓣api请求图书信息
         wx.request({
           method: 'GET',
@@ -105,7 +117,7 @@ Page({
           success: function(res) {
             wx.navigateTo({
               url: './addmybook/addmybook?image=' + res.data.image + '&title=' + res.data.title +
-                '&author=' + res.data.author[0]+'&isbn='+isbn,
+                '&author=' + res.data.author[0],
             })
             console.log(res.data.image)
             console.log(res.data.title)
